3bc1f655fd5c126167506e0ff0e98fd4b11e67f1
DESKTOP-GVQS5I4\Omelian Levkovych <omelianai@gmail.com>
2022-01-30 22:56:25 +0200

    Initial solution

---
+++ src/ImageService/ImageService/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImageService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet("/ping")]
+        public int Ping()
+        {
+            return 1;
+        }
+    }
+}
