20b4b2dafba6b9265b3533f6d6c1366fcae32f36
Omelian Levkovych <omelianai@gmail.com>
2022-01-31 00:27:07 +0200

    added temporary ping endpoint

---
+++ Application/Controllers/PingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> logger;
+
+        public PingController(ILogger<WeatherForecastController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet("/ping")]
+        public int GetPing()
+        {
+            logger.LogError("Test log!");
+            return 1;
+        }
+    }
+}
